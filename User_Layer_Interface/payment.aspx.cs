72299d90e6b24e4c2b63669a0f73069b07fcc46a
Camellia <pccamellia@gmail.com>
2023-08-12 20:30:27 +0800

    admin hash update & history page try

--- User_Layer_Interface/payment.aspx.cs
+++ User_Layer_Interface/payment.aspx.cs
@@ -15,7 +15,7 @@ namespace kuze
     public partial class Payment : System.Web.UI.Page
     {
 
-        private string connectionString = "Server=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=C:\\Users\\62895\\Source\\Repos\\kuzefinal\\App_Data\\KuzeDB.mdf;Trusted_Connection=True;";
+        private string connectionString = "Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=|DataDirectory|\\KuzeDB.mdf;Integrated Security=True";
         protected void Page_Load(object sender, EventArgs e)
         {
             if (Session["TotalCartPrice"] == null || Session["TotalCartItems"] == null)
