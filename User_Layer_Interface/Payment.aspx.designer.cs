10e95929d5b2e1f645dfc9211e38123981674180
VNicholas24 <nichsebu@gmail.com>
2023-08-13 12:15:52 +0800

    payment changes

--- User_Layer_Interface/Payment.aspx.designer.cs
+++ User_Layer_Interface/Payment.aspx.designer.cs
@@ -96,6 +96,15 @@ namespace kuze
         protected global::System.Web.UI.WebControls.Button btnSubmitPayment;
 
         /// <summary>
+        /// lblErrorMessage control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lblErrorMessage;
+
+        /// <summary>
         /// lblTotalItems control.
         /// </summary>
         /// <remarks>
